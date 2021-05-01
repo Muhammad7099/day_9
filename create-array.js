87394a19dded8cecb3bb5ec9b86a3bdceaf7bfd6
muhammad7099 <muhammad7099@mail.ru>
2021-05-01 11:27:44 +0300

    день девятый

---
+++ create-array.js
@@ -0,0 +1,23 @@
+//Пустой массив;
+let emptyArray = [];
+
+//Массив с тремя числовыми элементами;
+let numericElements = [30, 60, 90];
+
+//Массив с тремя строковыми элементами;
+let stringElements = ['first', 'second', 'third'];
+
+//Массив с тремя строковыми и с тремя числовыми элементами;
+let AnArrayWithThreeStringsAndThreeNumericElements = ['first', 'second', 'third', 30, 60, 90];
+
+//Массив с именами всех твоих одногруппников;
+let myClassmates = ['Deni', 'Eldar', 'Muhammad', 'Dzambulat', 'Tashu Hadzi'];
+
+//Массив, в котором есть число, строка;
+let anArrayContainingNumberString = [null, undefined, true, false];
+
+//Массив, внутри которого три пустых массива;
+let anArrayWithThreeEmptyArraysInside = ['', '', ''];
+
+//Массив, внутри которого два массива, а внутри каждого вложенного массива любые три числа.
+let nestedArrays = [['second', 'third'], [10, 20]];
