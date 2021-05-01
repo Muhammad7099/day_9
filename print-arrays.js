b26d09a908c3a2060179d087d72f87b44c9f41de
muhammad7099 <muhammad7099@mail.ru>
2021-05-01 14:53:21 +0300

    Закончил первую часть

---
+++ print-arrays.js
@@ -0,0 +1,70 @@
+//Пустой массив;
+let emptyArray = [];
+
+//После задачи A выведи в консоль первый элемент массива;
+//console.log(emptyArray[0]);
+
+//*******************************************************************
+
+//Массив с тремя числовыми элементами;
+let numericElements = [30, 60, 90];
+
+//После задачи B выведи в консоль сумму первого и третьего элеметов массива
+//console.log(numericElements[0] + numericElements[2]);
+
+//*******************************************************************
+
+//Массив с тремя строковыми элементами;
+let stringElements = ['first', 'second', 'third'];
+
+//после задачи C выведи в консоль строку, в которой склеены все три элемента массива;
+//console.log(stringElements[0] + stringElements[1] + stringElements[2]);
+
+//*******************************************************************
+
+//Массив с тремя строковыми и с тремя числовыми элементами;
+let AnArrayWithThreeStringsAndThreeNumericElements = ['first', 'second', 'third', 30, 60, 90];
+
+//После задачи D выведи в консоль такую строку:
+// 1-й элемент массива - [тут сам элемент],
+// а последний элемент - [тут сам элемент];
+//console.log(`1 - й элемент массива - ${AnArrayWithThreeStringsAndThreeNumericElements[0]} а последний элемент ${AnArrayWithThreeStringsAndThreeNumericElements[5]}`);
+
+//*******************************************************************
+
+//Массив с именами всех твоих одногруппников;
+let myClassmates = ['Deni', 'Eldar', 'Muhammad', 'Dzambulat', 'Tashu Hadzi'];
+
+//После задачи E выведи в консоль пятый элемент текущего массива и последний элемент массива из задания B;
+//console.log(myClassmates[4] + `-` + numericElements[2]);
+
+//*********************************************************************
+
+//Массив, в котором есть число, строка;
+// let anArrayContainingNumberString = [null, undefined, true, false];
+
+//после задачи F выведи каждый элемент массива в отдельный console.log();
+// console.log(anArrayContainingNumberString[0])
+// console.log(anArrayContainingNumberString[1])
+// console.log(anArrayContainingNumberString[2])
+// console.log(anArrayContainingNumberString[3])
+
+//*********************************************************************
+
+//Массив, внутри которого три пустых массива;
+// let anArrayWithThreeEmptyArraysInside = [[], [], []];
+
+//После задачи G выведи в консоль длину массива;
+// console.log(anArrayWithThreeEmptyArraysInside.length);
+
+//*********************************************************************
+
+//Массив, внутри которого два массива, а внутри каждого вложенного массива любые три числа.
+let nestedArrays = [[3, 2 ,8], [7, 9, 5]];
+
+//После задачи H выведи в консоль произведение второго элемента первого вложенного массива
+// и третьего элемента второго вложенного массива.
+//console.log(nestedArrays[0][1] * nestedArrays[1][2]);
+
+//*********************************************************************
+
